348f820493b3eb919e879f06bdf717f60ed9a04e
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-08-16 11:21:32 +0530

    Add files via upload

---
+++ 10.literal types/litrel.ts
@@ -0,0 +1,5 @@
+let color :"red"|"green"|"blue"|"yellow"|"crimson"|"white"|"black"|"purpel"|"violet";
+color="red";
+color="black";
+let password:'secretpassword'='secretpassword'
+console.log(password)
