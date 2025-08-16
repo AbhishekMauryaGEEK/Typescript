30328387ad461ee3ee60d8464eec6958136890db
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-08-16 11:21:59 +0530

    Add files via upload

---
+++ 12.Enums/enums.ts
@@ -0,0 +1,8 @@
+ enum icecream {
+    vanella="vanella",
+    chocklate="chocklate",
+     strawberry="sda",
+    darkforest="sdag"
+}
+console.log(icecream.darkforest)
+console.log(` i am eating ${icecream.chocklate}`)
